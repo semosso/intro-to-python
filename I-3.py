01bb8a9ee4606eb65dd33691eb33619b8142ac03
Vinicius Azambuja <vnazambuja@gmail.com>
2018-12-22 23:05:50 -0500

    list v dict

--- I-3.py
+++ I-3.py
@@ -10,7 +10,7 @@ cliches = [
 print(cliches[int(input("> "))]) # modificado do original para permitir input em vez de escolha fixa
 
 # The following is a DICTIONARY (collection of unique KEYS and their associated VALUES), another type data structure
-quotes = { # would it change if I used [] instead of {}?
+quotes = { # would it change if I used [] instead of {}? nope, error
     "Moe": "A wise guy, huh?", 
     "Larry": "Ow!",
     "Curly": "Nyuk nyuk!",
